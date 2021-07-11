9fbd77f82265ab36571482444424f47ebfdf0949
kaziNAHIAN <provat44.knhp@gmail.com>
2021-07-11 13:44:06 +0600

    Routing  done!

--- GenePrediction/GP/views.py
+++ GenePrediction/GP/views.py
@@ -9,7 +9,7 @@ def home(request):
 	return render(request,'GP/home.html')
 
 def predict(request):
-    record_iterator = SeqIO.parse("/home/nahian/Desktop/GenePrediction/GenePrediction/GP/NZ_GG770409.fna", "fasta")
+    record_iterator = SeqIO.parse(request.POST.get('path'),"fasta")
     record1 = next(record_iterator)
     sq = str(record1.seq)
     stopList = [i for i in range(len(sq)) if (sq.startswith('TGA',i) or sq.startswith('TAG',i) or sq.startswith('TAA',i))]
